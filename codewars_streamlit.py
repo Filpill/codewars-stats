51b480f917f181b8d71054f16e39188d2a83b6ac
Filip Livancic <filiplivancic@gmail.com>
2022-12-02 14:06:45 +0000

    replaced streamlit local files with s3 links

--- codewars_streamlit.py
+++ codewars_streamlit.py
@@ -1,15 +1,13 @@
+import s3fs
 import streamlit as st
 import pandas as pd
 import numpy as np
+from datetime import date
 import altair as alt
 import matplotlib.pyplot as plt
 
 st.title('Codewars Stats - Filpill')
-
-@st.cache(allow_output_mutation=True)
-def load_data(filename,nrows):
-    data = pd.read_csv(filename, nrows=nrows)
-    return data
+# @st.cache(allow_output_mutation=True)
 
 def alt_bar_chart(data,x_label,y_label,x_size,y_size):
     st.write(alt.Chart(data).mark_bar().encode(
@@ -27,24 +25,26 @@ def alt_bar_chart(data,x_label,y_label,x_size,y_size):
     ))
 
 # Loading in Raw Data
+date_str = date.today().strftime("%d-%b-%Y")
+st.text(f'Data refreshed: {date_str}')
 data_load_state = st.text('Loading data...')
-raw_data = load_data('csv/codewars_request.csv',10000)
+raw_data_df = pd.read_csv(f"s3://codewarsdata/codewars_request.csv",index_col=0)
 data_load_state.text("Done! (using st.cache)")
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
-    st.write(raw_data)
+    st.write(raw_data_df)
 
 # Creating Monthly Count
 st.subheader('Monthly Count')
-monthly = load_data('csv/monthly.csv',100)
+monthly = pd.read_csv(f"s3://codewarsdata/monthly.csv",index_col=0)
 [x_label,y_label] = ['Completed Date','Count']
 [x_size,y_size] = [800,300]
 alt_bar_chart(monthly,x_label,y_label,x_size,y_size) 
 
 # Rank Count
 st.subheader('Rank Count')
-rank = load_data('csv/rank.csv',100)
+rank = pd.read_csv(f"s3://codewarsdata/rank.csv",index_col=0)
 [x_label,y_label] = ['Rank','Count']
 [x_size,y_size] = [800,300]
 st.write(alt.Chart(rank).mark_bar().encode(
@@ -60,12 +60,11 @@ st.write(alt.Chart(rank).mark_bar().encode(
     width = x_size,
     height = y_size
 ))
-# alt_bar_chart(rank,x_label,y_label,x_size,y_size) 
 
 # Category Count
 top = 20 #Taking top 20 categories
 st.subheader(f'Category Count - Top {top}')
-category = load_data('csv/tags.csv',100)
+category = pd.read_csv(f"s3://codewarsdata/tags.csv",index_col=0)
 category = category.iloc[::-1]
 category = category.iloc[0:top] 
 [x_label,y_label] = ['Tags','Count']
@@ -74,7 +73,7 @@ alt_bar_chart(category,x_label,y_label,x_size,y_size)
 
 # Language Count
 st.subheader('Language Count')
-language = load_data('csv/language.csv',100)
+language = pd.read_csv(f"s3://codewarsdata/language.csv",index_col=0)
 [x_label,y_label] = ['Languages','Count']
 [x_size,y_size] = [800,300]
 alt_bar_chart(language,x_label,y_label,x_size,y_size) 
